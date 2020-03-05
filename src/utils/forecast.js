7b3bec4531313a7b5937152b2f9981eee299c2af
Ritchell Echual <x119053@corp.ads>
2020-03-05 03:53:16 -0800

    Add forcast strings

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,10 @@
 const request = require('request')
+const moment = require('moment')
 
 const forecast = (latitude, longtitude, callback) => {
     const url = 'https://api.darksky.net/forecast/4857f352b6dacd65654fce3a5e933ec2/' + latitude + ',' + longtitude
+
+    let time = ''
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,12 +12,15 @@ const forecast = (latitude, longtitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
+            time = moment(body.currently.time)
             callback(undefined, body.daily.data[0].summary +
-                '  It is currently ' + body.currently.temperature +
+                '  The time is ' + time.format("h:mm:ss a") +
+                '. It is currently ' + body.currently.temperature +
                 ' degress out.  There is a ' + body.currently.precipProbability +
-                '% chance of rain.')
+                '% chance of rain.  Humidity is ' + body.daily.data[0].humidity)
         }
     })
 }
+
 
 module.exports = forecast

ab276cd2aa740272b5440a548d00c365fc47f3d7
Ritchell Echual <x119053@corp.ads>
2020-03-05 00:21:10 -0800

    Remove unecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side javascript side is loaded')
+// console.log('Client side javascript side is loaded')
 
 const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
